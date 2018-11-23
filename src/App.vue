8b8d26384b433b998686ebea0109c37a77bb5e17
wang <969972331@qq.com>
2018-11-23 17:19:17 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template lang="pug">
+  <!-- <div id="app">
+    <img src="./assets/logo.png">
+    <HelloWorld/>
+    <h1>欢迎来到vue的世界</h1>
+  </div> -->
+  div#app
+    img(src="./assets/logo.png")
+    h1 Welcome from pug & sass
+
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style lang="scss">
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+  h1{
+    color: #42b983;
+  }
+}
+</style>
