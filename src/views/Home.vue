a5dd8e779652ffc1ecc8d3125eb101d9555a05c1
wang <969972331@qq.com>
2018-11-26 15:09:07 +0800

    完成登录功能，优化路由配置

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -24,6 +24,11 @@ export default {
           case 'Register':
             vm.showMsg('注册成功')
             break
+
+          case 'Login':
+          // 显示登录成功
+          vm.showMsg('登录成功')
+          break
         }
       } else if (logout) {
         vm.showMsg('操作成功')
