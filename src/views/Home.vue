178ce336b55efbdd303d8ac8eda727aa7179c9dd
wang <969972331@qq.com>
2018-12-04 21:54:12 +0800

    首页展示帖子列表

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,60 +1,110 @@
 <template>
   <div>
     <Message :show.sync="msgShow" :type="msgType" :msg="msg"/>
+
+    <!-- 帖子列表 -->
+    <div class="col-md-9 topics-index main-col">
+      <div class="panel panel-default">
+        <div class="panel-heading">
+          <ul class="list-inline topic-filter">
+            <li><a href="/topics?filter=default" class="active">活跃</a></li>
+            <li><a href="/topics?filter=excellent">精华</a></li>
+            <li><a href="/topics?filter=vote">投票</a></li>
+            <li><a href="/topics?filter=recent">最近</a></li>
+            <li><a href="/topics?filter=noreply">零回复</a></li>
+          </ul>
+          <div class="clearfix"></div>
+        </div>
+
+        <div class="panel-body remove-padding-horizontal">
+          <ul class="list-group row topic-list">
+            <li v-for="article in articles" :key="article.articleId" class="list-group-item">
+              <router-link :to="`/articles/${article.articleId}/content`" tag="div" class="reply_count_area hidden-xs pull-right">
+                <div class="count_set">
+                  <span class="count_of_votes" title="投票数">{{ article.likeUsers ? article.likeUsers.length : 0 }}</span>
+                  <span class="count_seperator">/</span>
+                  <span class="count_of_replies" title="回复数">{{ article.comments ? article.comments.length : 0 }}</span>
+                  <span class="count_seperator">|</span>
+                  <abbr class="timeago">{{ article.date | moment('from') }}</abbr>
+                </div>
+              </router-link>
+              <router-link v-if="user" :to="`/${user.name}`" tag="div" class="avatar pull-left">
+                <img :src="user.avatar" class="media-object img-thumbnail avatar avatar-middle">
+              </router-link>
+              <router-link :to="`/articles/${article.articleId}/content`" tag="div" class="infos">
+                <div class="media-heading">
+                  {{ article.title }}
+                </div>
+              </router-link>
+            </li>
+          </ul>
+        </div>
+
+      </div>
+    </div>
+
   </div>
 </template>
 
 <script>
-export default {
-  name: 'Home',
-  data() {
-    return {
-      msg: '', // 消息
-      msgType: '', // 消息类型
-      msgShow: false // 是否显示消息，默认不显示
-    }
-  },
-  beforeRouteEnter(to, from, next) {
-    const fromName = from.name
-    const logout = to.params.logout
 
-    next(vm => {
-      if (vm.$store.state.auth) {
-        switch (fromName) {
-          case 'Register':
-            vm.showMsg('注册成功')
+  import { mapState } from 'vuex'
+
+  export default {
+    name: 'Home',
+    data() {
+      return {
+        msg: '', // 消息
+        msgType: '', // 消息类型
+        msgShow: false // 是否显示消息，默认不显示
+      }
+    },
+    beforeRouteEnter(to, from, next) {
+      const fromName = from.name
+      const logout = to.params.logout
+
+      next(vm => {
+        if (vm.$store.state.auth) {
+          switch (fromName) {
+            case 'Register':
+              vm.showMsg('注册成功')
+              break
+
+            case 'Login':
+            // 显示登录成功
+            vm.showMsg('登录成功')
             break
-
-          case 'Login':
-          // 显示登录成功
-          vm.showMsg('登录成功')
-          break
+          }
+        } else if (logout) {
+          vm.showMsg('操作成功')
         }
-      } else if (logout) {
-        vm.showMsg('操作成功')
+      })
+    },
+    computed: {
+      auth() {
+        return this.$store.state.auth
+      },
+      ...mapState([
+        'auth',
+        'user',
+        'articles'
+     ])
+    },
+    watch: {
+      auth(value) {
+        if (!value) {
+          this.showMsg('操作成功')
+        }
       }
-    })
-  },
-  computed: {
-    auth() {
-      return this.$store.state.auth
-    }
-  },
-  watch: {
-    auth(value) {
-      if (!value) {
-        this.showMsg('操作成功')
+    },
+    methods: {
+      showMsg(msg, type = 'success') {
+        this.msg = msg
+        this.msgType = type
+        this.msgShow = true
       }
-    }
-  },
-  methods: {
-    showMsg(msg, type = 'success') {
-      this.msg = msg
-      this.msgType = type
-      this.msgShow = true
     }
   }
-}
 </script>
 
 <style scoped>
